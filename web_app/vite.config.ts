cd3b82769005695cf1deb5be3ae984f6a350034b
julesntare <julesntare@gmail.com>
2025-10-21 00:09:44 +0200

    added netlify plugin for deployment purpose

--- web_app/vite.config.ts
+++ web_app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import netlify from "@netlify/vite-plugin";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -8,7 +9,7 @@ export default defineConfig({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
